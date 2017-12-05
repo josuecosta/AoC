d1974cf845a65a5c34539a7e1625960825670d53
Josué <josuecosta@ua.pt>
2017-12-05 09:35:23 +0000

    Update Day5.cs

--- AoC17/AoC/Day5.cs
+++ AoC17/AoC/Day5.cs
@@ -7,14 +7,30 @@
         public void Run()
         {
             // Part 1
-            foreach (var row in this.Input)
+            var position = 0;
+            var steps = 0;
+            var offSets = this.Input.Select(o => int.Parse(o)).ToArray();
+            while (position < offSets.Length)
             {
+                var temp = offSets[position];
+                offSets[position]++;
+                position = position + temp;
+                steps++;
             }
+            this.Output1 = steps;
 
             // Part 2
-            foreach (var row in this.Input)
+            position = 0;
+            steps = 0;
+            offSets = this.Input.Select(o => int.Parse(o)).ToArray();
+            while (position < offSets.Length)
             {
+                var temp = offSets[position];
+                offSets[position] = temp >= 3 ? offSets[position] - 1 : offSets[position] + 1;
+                position = position + temp;
+                steps++;
             }
+            this.Output2 = steps;
         }
     }
 }
