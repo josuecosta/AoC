1c8ecff1ac3542776aeb22cb7236a45c83fc4009
josuecosta <josuecosta@ua.pt>
2017-12-08 17:44:27 +0000

    Prepare Day9.cs

---
+++ AoC17/AoC/Day9.cs
@@ -0,0 +1,18 @@
+﻿namespace AoC17
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Day9 : Master
+    {
+        public void Run()
+        {
+            // Part 1
+            foreach (var row in this.Input)
+            {
+                
+            }
+        }
+    }
+}
