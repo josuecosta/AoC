79d265fe864377fdab973906d51c67fc8c5013c1
Josué Costa <josue.scosta@yazaki-europe.com>
2017-12-13 19:00:35 +0000

    Prepare Day 14

---
+++ AoC17/AoC/Day14.cs
@@ -0,0 +1,24 @@
+﻿namespace AoC17
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Day14 : Master
+    {
+        private IDictionary<int, int> dic = new Dictionary<int, int>();
+
+        public void Run()
+        {
+            foreach (var row in this.Input)
+            {
+            }
+
+            // Part 1
+            this.Output1 = 1;
+
+            // Part 2
+            this.Output2 = 1;
+        }
+    }
+}
--- AoC17/Program.cs
+++ AoC17/Program.cs
@@ -6,7 +6,7 @@
     {
         public static void Main(string[] args)
         {
-            var AoC = new Day3();
+            var AoC = new Day14();
             AoC.Day = int.Parse(AoC.GetType().Name.Substring(3));
             AoC.ReadInput();
             AoC.Run();
