d37b7bfa7ef5390ce86015ee5100b122b8fdf28c
josuecosta <josuecosta@ua.pt>
2017-12-19 23:21:22 +0000

    Complete Day 17 Part 1

--- AoC17/AoC/Day17.cs
+++ AoC17/AoC/Day17.cs
@@ -9,9 +9,44 @@
     {
         public void Run()
         {
-            this.Output1 = 1;
+            //var steps = int.Parse(this.Input.First());
+            var steps = 3;
 
-            this.Output2 = 2;
+            var buffer = new List<int> {0};
+            var position = 0;
+            var counter = 0;
+
+            for (int i = 0; i < 2017; i++)
+            {
+                position = (steps + position) % buffer.Count;
+
+                if (++position == buffer.Count)
+                {
+                    buffer.Add(++counter);
+                    continue;
+                }
+                buffer.Insert(position, ++counter);
+            }
+
+            this.Output1 = buffer[((position+1)%buffer.Count)];
+
+            //buffer = new List<int> { 0 };
+            //position = 0;
+            //counter = 0;
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    position = (steps + position) % buffer.Count;
+
+            //    if (++position == buffer.Count)
+            //    {
+            //        buffer.Add(++counter);
+            //        continue;
+            //    }
+            //    buffer.Insert(position, ++counter);
+            //    Console.WriteLine(string.Join(" ", buffer));
+            //}
+
+            this.Output2 = buffer[1];
         }
     }
 }
