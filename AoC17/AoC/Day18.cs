f0089d8bd40b9bf45c782add0b1623e3469540a6
Josué Costa <josue.scosta@yazaki-europe.com>
2017-12-20 15:11:40 +0000

    Prepare Day 18

---
+++ AoC17/AoC/Day18.cs
@@ -0,0 +1,16 @@
+﻿namespace AoC17
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    internal class Day18 : Master
+    {
+        public void Run()
+        {
+            this.Output1 = 1;
+            this.Output2 = 1;
+        }
+    }
+}
