5e44688a74651ac2bae56b9d379c7db4b17fcbd7
Josué Costa <josue.scosta@yazaki-europe.com>
2017-12-26 11:35:55 +0000

    Prepare Day 19

---
+++ AoC17/AoC/Day19.cs
@@ -0,0 +1,13 @@
+﻿namespace AoC17
+{
+    using System.Linq;
+
+    public class Day19 : Master
+    {
+        public void Run()
+        {
+            this.Output1 = 0;
+            this.Output2 = 0;
+        }
+    }
+}
