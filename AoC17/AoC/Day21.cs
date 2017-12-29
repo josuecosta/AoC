ee4da9fb6d4c766f914e60b79723a81fc13e9acf
Josué Costa <josue.scosta@yazaki-europe.com>
2017-12-29 16:25:03 +0000

    Prepare Day 21

---
+++ AoC17/AoC/Day21.cs
@@ -0,0 +1,15 @@
+﻿namespace AoC17
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Day21 : Master
+    {
+        public void Run()
+        {
+            this.Output1 = 1;
+            this.Output2 = 1;
+        }
+    }
+}
--- AoC17/Program.cs
+++ AoC17/Program.cs
@@ -6,7 +6,7 @@
     {
         public static void Main(string[] args)
         {
-            var AoC = new Day20();
+            var AoC = new Day21();
             AoC.Day = int.Parse(AoC.GetType().Name.Substring(3));
             AoC.ReadInput();
             AoC.Run();
